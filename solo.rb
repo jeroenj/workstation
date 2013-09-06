dc70f38e4297b6149d9785df210ce877049fa722
Jeroen Jacobs <git@jeroenj.be>
2013-09-06 20:16:48 +0200

    Set data_bag_path

--- solo.rb
+++ solo.rb
@@ -1,3 +1,4 @@
 cookbook_path File.expand_path('../cookbooks', __FILE__)
 role_path File.expand_path('../roles', __FILE__)
+data_bag_path File.expand_path('../data_bags', __FILE__)
 json_attribs File.expand_path('../node.json', __FILE__)
