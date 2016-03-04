0326e6ecf54b88918c1a5b22512e6af1f13a1a4b
Jeroen Jacobs <git@jeroenj.be>
2016-03-04 23:22:13 +0100

    [ruby] Installs bundler-audit ~> 0.5.0

--- site-cookbooks/base/attributes/ruby.rb
+++ site-cookbooks/base/attributes/ruby.rb
@@ -1,6 +1,6 @@
 default[:base][:ruby][:gems] = [
   { name: 'bundler', version: '~> 1.11.2' },
-  { name: 'bundler-audit', version: '~> 0.4.0' },
+  { name: 'bundler-audit', version: '~> 0.5.0' },
   { name: 'cheat' },
   { name: 'explain_shell' },
   { name: 'pry', version: '~> 0.10.0' }
