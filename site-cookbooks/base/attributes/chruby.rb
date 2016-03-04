fa3bacf6dc9c667731f59e4ea585334937477788
Jeroen Jacobs <git@jeroenj.be>
2016-03-04 23:21:53 +0100

    [chruby] Installs bundler-audit ~> 0.5.0

--- site-cookbooks/base/attributes/chruby.rb
+++ site-cookbooks/base/attributes/chruby.rb
@@ -9,7 +9,7 @@ default[:base][:chruby] = {
   default: '2.2.4',
   gems: [
     { name: 'bundler', version: '~> 1.11.2' },
-    { name: 'bundler-audit', version: '~> 0.4.0' },
+    { name: 'bundler-audit', version: '~> 0.5.0' },
     { name: 'cheat' },
     { name: 'explain_shell' },
     { name: 'foreman' },
