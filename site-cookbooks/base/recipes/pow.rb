5d328c197f9692ec766c33acb7e8c40a37c125d1
Jeroen Jacobs <git@jeroenj.be>
2016-03-29 10:15:28 +0200

    [pow] Uses not_if blocks instead of a ruby if to skip resources if pow already is installed

--- site-cookbooks/base/recipes/pow.rb
+++ site-cookbooks/base/recipes/pow.rb
@@ -2,28 +2,33 @@ pow_installed = ::File.exist? '/usr/local/bin/pow'
 
 package 'pow'
 
-unless pow_installed
-  # Homebrew does not take care of this (yet)
-  # https://github.com/37signals/pow/issues/298
-  pow_hosts = "#{ENV['HOME']}/Library/Application Support/Pow/Hosts"
-  base_recursive_directory pow_hosts do
-    owner node[:base][:username]
-    group node[:base][:group]
-  end
-  link "#{ENV['HOME']}/.pow" do
-    to pow_hosts
-    owner node[:base][:username]
-    group node[:base][:group]
-  end
+# Homebrew does not take care of this (yet)
+# https://github.com/37signals/pow/issues/298
+pow_hosts = "#{ENV['HOME']}/Library/Application Support/Pow/Hosts"
+base_recursive_directory pow_hosts do
+  owner node[:base][:username]
+  group node[:base][:group]
+end
+link "#{ENV['HOME']}/.pow" do
+  to pow_hosts
+  owner node[:base][:username]
+  group node[:base][:group]
+end
 
-  execute 'pow --install-system'
-  execute 'pow --install-local' do
-    user node[:base][:username]
-    group node[:base][:group]
-  end
-  execute 'launchctl load -w /Library/LaunchDaemons/cx.pow.firewall.plist'
-  execute 'launchctl load -w ~/Library/LaunchAgents/cx.pow.powd.plist' do
-    user node[:base][:username]
-    group node[:base][:group]
-  end
+execute 'pow --install-system' do
+  not_if { pow_installed }
+end
+execute 'pow --install-local' do
+  user node[:base][:username]
+  group node[:base][:group]
+  not_if { pow_installed }
+end
+
+execute 'launchctl load -w /Library/LaunchDaemons/cx.pow.firewall.plist' do
+  not_if { pow_installed }
+end
+execute 'launchctl load -w ~/Library/LaunchAgents/cx.pow.powd.plist' do
+  user node[:base][:username]
+  group node[:base][:group]
+  not_if { pow_installed }
 end
