eea06a2dc362746f8a6b9682f08b413e870e20fd
Jeroen Jacobs <git@jeroenj.be>
2016-04-21 22:25:50 +0200

    [transmission] Hides STDERR when trying to set the remote password
    Also marks it as sensitive to avoid clear text passwords in Chef's log.

--- site-cookbooks/base/recipes/transmission.rb
+++ site-cookbooks/base/recipes/transmission.rb
@@ -2,5 +2,6 @@ homebrew_cask 'transmission'
 
 execute 'Set remote access password' do
   command "security unlock #{ENV['HOME']}/Library/Keychains/login.keychain && security add-generic-password -a Remote -s Transmission:Remote -T /Applications/Transmission.app -w #{node[:base][:transmission][:remote_access_password]} #{ENV['HOME']}/Library/Keychains/login.keychain"
-  not_if { system 'security find-generic-password -s Transmission:Remote > /dev/null' }
+  sensitive true
+  not_if 'security find-generic-password -s Transmission:Remote > /dev/null 2>&1'
 end
