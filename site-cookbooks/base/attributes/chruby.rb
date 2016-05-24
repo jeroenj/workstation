6284818bf622971ecbcf70220c31e0e9ae32a837
Jeroen Jacobs <git@jeroenj.be>
2016-05-24 10:17:54 +0200

    [chruby] Adds ruby 2.2.2 and 2.3.1 and makes 2.3.1 the default ruby version

--- site-cookbooks/base/attributes/chruby.rb
+++ site-cookbooks/base/attributes/chruby.rb
@@ -1,12 +1,14 @@
 default[:base][:chruby] = {
   path: '/opt/rubies',
   rubies: %w(
+    2.3.1
     2.3.0
     2.2.4
+    2.2.2
     2.1.8
     2.0.0-p648
   ),
-  default: '2.2.4',
+  default: '2.3.1',
   gems: [
     { name: 'bundler', version: '~> 1.11.2' },
     { name: 'bundler-audit', version: '~> 0.5.0' },
