4483dbbc2754e3ad09da9fd37f4a54897dc1fd47
Jeroen Jacobs <git@jeroenj.be>
2016-06-15 10:30:03 +0200

    [cyberduck] Removes recipe

--- site-cookbooks/base/attributes/cyberduck.rb
+++
@@ -1,9 +0,0 @@
-default[:base][:cyberduck][:bookmarks] = []
-
-default[:base][:settings][:cyberduck] = {
-  'domain' => 'ch.sudo.cyberduck',
-  'bookmark.icon.size' => 32,
-  'browser.alternatingRows' => true,
-  'browser.horizontalLines' => false,
-  'connection.protocol.default' => 'sftp'
-}
