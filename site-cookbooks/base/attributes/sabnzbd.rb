e5c2958de851ef7712b2a3ed76002c69999de6a0
Jeroen Jacobs <git@jeroenj.be>
2016-06-15 10:51:58 +0200

    [sabnzbd] Removes recipe

--- site-cookbooks/base/attributes/sabnzbd.rb
+++
@@ -1,7 +0,0 @@
-default[:base][:sabnzbd][:preferences][:https] = 0
-default[:base][:sabnzbd][:preferences][:https_port] = 9090
-default[:base][:sabnzbd][:preferences][:https_key] = '""'
-default[:base][:sabnzbd][:preferences][:https_cert] = '""'
-default[:base][:sabnzbd][:preferences][:osx_speed] = 0
-default[:base][:sabnzbd][:preferences][:schedule] = []
-default[:base][:sabnzbd][:preferences][:servers] = []
