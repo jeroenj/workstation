2add8185bfd8e506514f6ae7fe53f964fffd8e5f
Jeroen Jacobs <git@jeroenj.be>
2016-06-15 10:30:42 +0200

    [cord] Removes recipe

--- site-cookbooks/base/attributes/cord.rb
+++
@@ -1,6 +0,0 @@
-default[:base][:settings][:cord] = {
-  'domain' => 'net.sf.cord',
-  'CRDBaseConnectionScreenSize' => '1280x800',
-  'SUEnableAutomaticChecks' => true,
-  'SUCheckAtStartup' => true
-}
