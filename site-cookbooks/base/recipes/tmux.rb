952f5e6f36bcce0ab9b61bd098c757bb8dd22844
Jeroen Jacobs <git@jeroenj.be>
2016-10-06 17:11:49 +0200

    [tmux] Fixes copy-paste on macOS Sierra
    https://github.com/tmux/tmux/issues/543
    
    This does not yet fix copy-paste from tmux buffers.

--- site-cookbooks/base/recipes/tmux.rb
+++ site-cookbooks/base/recipes/tmux.rb
@@ -1,5 +1,8 @@
 package 'tmux'
 
+# Fix copy-paste on macOS Sierra
+package 'reattach-to-user-namespace'
+
 cookbook_file "#{ENV['HOME']}/.tmux.conf" do
   source 'tmux/tmux.conf'
   owner node[:base][:username]
