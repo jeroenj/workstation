df97d447509742bb990da53c132cde22f963aeaf
Jeroen Jacobs <git@jeroenj.be>
2016-11-22 11:18:01 +0100

    [dash] Adds license

--- site-cookbooks/base/attributes/dash.rb
+++ site-cookbooks/base/attributes/dash.rb
@@ -1,3 +1,10 @@
+default[:base][:dash][:license][:name] = nil
+default[:base][:dash][:license][:email] = nil
+default[:base][:dash][:license][:timestamp] = nil
+default[:base][:dash][:license][:apple_order_id] = nil
+default[:base][:dash][:license][:version] = nil
+default[:base][:dash][:license][:signature] = nil
+
 default[:base][:settings][:dash] = {
   'domain' => 'com.kapeli.dash',
   'DHNotificationDocsetPressEnterOrClickIconTip' => true,
--- site-cookbooks/base/recipes/dash.rb
+++ site-cookbooks/base/recipes/dash.rb
@@ -1,1 +1,16 @@
 # include base::settings at the end of the run list to include settings
+
+homebrew_cask 'dash'
+
+if node[:base][:dash][:license][:signature]
+  directory "#{ENV['HOME']}/Library/Application Support/Dash/License" do
+    owner node[:base][:username]
+    group node[:base][:group]
+  end
+
+  template "#{ENV['HOME']}/Library/Application Support/Dash/License/license.dash-license" do
+    source 'dash/license.dash-license.erb'
+    owner node[:base][:username]
+    group node[:base][:group]
+  end
+end
