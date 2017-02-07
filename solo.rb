705822f7602bdd55a2dc3a4f562cdfc2a77d6f85
Jeroen Jacobs <git@jeroenj.be>
2017-02-07 09:43:04 +0100

    Uses ./.chef/local-mode-cache instead of placing that cache in the project's root directory

--- solo.rb
+++ solo.rb
@@ -4,3 +4,4 @@ cookbook_path [pwd.join('berks-cookbooks').to_s, pwd.join('site-cookbooks').to_s
 role_path pwd.join('roles').to_s
 data_bag_path pwd.join('data_bags').to_s
 json_attribs pwd.join('node.json').to_s
+file_cache_path pwd.join('.chef/local-mode-cache').to_s
