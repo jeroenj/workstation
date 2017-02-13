c71af75cb9c6c9603cf303941bd6b01fa060e180
Jeroen Jacobs <git@jeroenj.be>
2017-02-13 15:30:44 +0100

    Disables saving updated nodes locally
    Skips
    https://github.com/chef/chef/blob/af1332711139cade37104169e943e290c6c00362/lib/chef/client.rb#L538-L547

--- solo.rb
+++ solo.rb
@@ -6,3 +6,5 @@ data_bag_path pwd.join('data_bags').to_s
 json_attribs pwd.join('node.json').to_s
 file_cache_path pwd.join('.chef/local-mode-cache').to_s
 file_backup_path pwd.join('.chef/local-mode-cache/backup').to_s
+
+Chef::Client.send(:define_method, :save_updated_node) { Chef::Log.debug("Skipping final node save because it is monkeypatched in solo.rb.") }
