698455c1a541eb2aae6e3264d9068916f1d800f9
Jeroen Jacobs <git@jeroenj.be>
2017-02-13 16:59:30 +0100

    [chef] Uses Openminds repository to manage ~/.chef

--- site-cookbooks/base/recipes/chef.rb
+++ site-cookbooks/base/recipes/chef.rb
@@ -2,28 +2,35 @@ homebrew_cask 'chefdk'
 
 chef_gem 'knife-solo'
 
-directory "#{ENV['HOME']}/.chef" do
+link "#{ENV['HOME']}/.chef" do
+  to "#{ENV['HOME']}/Development/openminds/chef-scripts/chef-dotfiles"
   owner node[:base][:username]
   group node[:base][:group]
 end
 
-%w(client.pem client.pub validation.pem).each do |filename|
-  file "#{ENV['HOME']}/.chef/#{filename}" do
-    content node[:base][:chef][filename.sub(/\./, '_')]
-    mode 0600
-    owner node[:base][:username]
-    group node[:base][:group]
-  end
-end
-
-template "#{ENV['HOME']}/.chef/knife.rb" do
-  source 'chef/knife.rb.erb'
-  variables({
-    cookbook_copyright: node[:base][:chef][:knife][:cookbook_copyright],
-    cookbook_email: node[:base][:chef][:knife][:cookbook_email],
-    cookbook_path: node[:base][:chef][:knife][:cookbook_path],
-    node_name: node[:base][:chef][:knife][:node_name]
-  })
-  owner node[:base][:username]
-  group node[:base][:group]
-end
+# Disabled as ~/.chef is managed by the Openminds repository.
+# directory "#{ENV['HOME']}/.chef" do
+#   owner node[:base][:username]
+#   group node[:base][:group]
+# end
+#
+# %w(client.pem client.pub validation.pem).each do |filename|
+#   file "#{ENV['HOME']}/.chef/#{filename}" do
+#     content node[:base][:chef][filename.sub(/\./, '_')]
+#     mode 0600
+#     owner node[:base][:username]
+#     group node[:base][:group]
+#   end
+# end
+#
+# template "#{ENV['HOME']}/.chef/knife.rb" do
+#   source 'chef/knife.rb.erb'
+#   variables({
+#     cookbook_copyright: node[:base][:chef][:knife][:cookbook_copyright],
+#     cookbook_email: node[:base][:chef][:knife][:cookbook_email],
+#     cookbook_path: node[:base][:chef][:knife][:cookbook_path],
+#     node_name: node[:base][:chef][:knife][:node_name]
+#   })
+#   owner node[:base][:username]
+#   group node[:base][:group]
+# end
