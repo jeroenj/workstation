5b3f96c1287c0eddcae4cd13f7f1d1304406c4b5
Jeroen Jacobs <git@jeroenj.be>
2017-02-13 15:14:31 +0100

    Sets file_backup_path to .chef/local-mode-cache/backup

--- solo.rb
+++ solo.rb
@@ -5,3 +5,4 @@ role_path pwd.join('roles').to_s
 data_bag_path pwd.join('data_bags').to_s
 json_attribs pwd.join('node.json').to_s
 file_cache_path pwd.join('.chef/local-mode-cache').to_s
+file_backup_path pwd.join('.chef/local-mode-cache/backup').to_s
