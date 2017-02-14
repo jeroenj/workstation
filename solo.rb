0e85cc0ca9f043e5b7ff2324e9718e999ca5099c
Jeroen Jacobs <git@jeroenj.be>
2017-02-14 15:28:47 +0100

    Redirects Chef logs to /var/log/chef.log
    Since v12.18.31 (I think) Chef started logging to STDOUT causing
    everything to appear twice in the output of `chef-client`.

--- solo.rb
+++ solo.rb
@@ -6,5 +6,7 @@ data_bag_path pwd.join('data_bags').to_s
 json_attribs pwd.join('node.json').to_s
 file_cache_path pwd.join('.chef/local-mode-cache').to_s
 file_backup_path pwd.join('.chef/local-mode-cache/backup').to_s
+log_level :info
+log_location pwd.join('/var/log/chef.log')
 
 Chef::Client.send(:define_method, :save_updated_node) { Chef::Log.debug("Skipping final node save because it is monkeypatched in solo.rb.") }
