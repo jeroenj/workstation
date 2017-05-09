2f6295305542331785a35b558fb7c8aa85dc7c18
Jeroen Jacobs <git@jeroenj.be>
2017-05-09 21:35:07 +0200

    [brew] Uses homebrew::install_taps recipe to install homebrew taps

--- site-cookbooks/base/recipes/brew.rb
+++ site-cookbooks/base/recipes/brew.rb
@@ -1,18 +1,9 @@
 include_recipe 'homebrew'
 
-taps = %w(
-  caskroom/cask
-  caskroom/eid
-  caskroom/versions
-)
-
-taps.each do |tap|
-  homebrew_tap tap
-end
-
 base_recursive_directory '/usr/local/var/log' do
   owner node[:base][:username]
   group 'admin'
 end
 
+include_recipe 'homebrew::install_taps'
 include_recipe 'homebrew::install_formulas'
