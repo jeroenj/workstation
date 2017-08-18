f4bb6c9513e906be8d2a4b1feed13f1ab1723ad6
Jeroen Jacobs <git@jeroenj.be>
2017-08-18 16:56:26 +0200

    [vim] Adds dein setup

--- site-cookbooks/base/recipes/vim.rb
+++ site-cookbooks/base/recipes/vim.rb
@@ -12,3 +12,19 @@ remote_directory config_dir do
   owner node[:base][:username]
   group node[:base][:group]
 end
+
+dein_dir = "#{ENV['HOME']}/.local/share/dein/repos/github.com/Shougo/dein.vim"
+
+base_recursive_directory dein_dir do
+  owner node[:base][:username]
+  group node[:base][:group]
+end
+
+git dein_dir do
+  repository 'https://github.com/Shougo/dein.vim'
+  action :checkout
+  checkout_branch 'master'
+  enable_checkout false
+  user node[:base][:username]
+  group node[:base][:group]
+end
