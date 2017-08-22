73035f25d296cf5a748f3b5dabff01650b4523ea
Jeroen Jacobs <git@jeroenj.be>
2017-08-22 11:31:22 +0200

    [ruby] Installs bundler ~> 1.15.4

--- site-cookbooks/base/attributes/ruby.rb
+++ site-cookbooks/base/attributes/ruby.rb
@@ -1,5 +1,5 @@
 default[:base][:ruby][:gems] = [
-  { name: 'bundler', version: '~> 1.15.0' },
+  { name: 'bundler', version: '~> 1.15.4' },
   { name: 'bundler-audit', version: '~> 0.5.0' },
   { name: 'cheat' },
   { name: 'explain_shell' },
