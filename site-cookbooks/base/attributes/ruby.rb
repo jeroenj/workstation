1b133e0617f1b86f65d90c85c13edc1602592726
Jeroen Jacobs <git@jeroenj.be>
2017-09-22 15:13:23 +0200

    [ruby] Install pry ~> v0.11.0

--- site-cookbooks/base/attributes/ruby.rb
+++ site-cookbooks/base/attributes/ruby.rb
@@ -3,5 +3,5 @@ default[:base][:ruby][:gems] = [
   { name: 'bundler-audit', version: '~> 0.5.0' },
   { name: 'cheat' },
   { name: 'explain_shell' },
-  { name: 'pry', version: '~> 0.10.0' }
+  { name: 'pry', version: '~> 0.11.0' }
 ]
