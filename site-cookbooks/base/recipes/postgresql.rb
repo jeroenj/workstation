f5c65162e9ae95d2a6a931ba4bc138b90dd9f968
Jeroen Jacobs <git@jeroenj.be>
2017-12-13 09:32:41 +0100

    [postgresql] Removes LaunchAgent

--- site-cookbooks/base/recipes/postgresql.rb
+++ site-cookbooks/base/recipes/postgresql.rb
@@ -1,7 +1,1 @@
 package 'postgresql'
-
-link "#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.postgresql.plist" do
-  to '/usr/local/opt/postgresql/homebrew.mxcl.postgresql.plist'
-  owner node[:base][:username]
-  group node[:base][:group]
-end
