50060fa2a562a9c7f43c3033899fe157dc300423
Jeroen Jacobs <git@jeroenj.be>
2018-12-13 21:58:33 +0100

    [safari] Uses base_plist_settings resource instead of plist_settings recipe for settings and uses the correct plist file

--- site-cookbooks/base/attributes/safari.rb
+++ site-cookbooks/base/attributes/safari.rb
@@ -1,15 +1,16 @@
-default[:base][:settings][:safari] = {
-  'domain' => 'com.apple.safari',
-  'AutoFillPasswords' => false,
-  'AutoOpenSafeDownloads' => false,
-  'DownloadsClearingPolicy' => 1,
-  'FindOnPageMatchesWordStartsOnly' => false,
-  'HistoryAgeInDaysLimit' => 365000,
-  'HomePage' => '',
-  'IncludeDevelopMenu' => true,
-  'NewWindowBehavior' => 4,
-  'SendDoNotTrackHTTPHeader' => true,
-  'ShowFavoritesBar' => false,
-  'ShowFullURLInSmartSearchField' => true,
-  'TabCreationPolicy' => 2
+default[:base][:safari][:plist_settings] = {
+  container: 'com.apple.Safari',
+  settings: {
+    'AutoFillPasswords' => false,
+    'AutoOpenSafeDownloads' => false,
+    'DownloadsClearingPolicy' => 1,
+    'FindOnPageMatchesWordStartsOnly' => false,
+    'HistoryAgeInDaysLimit' => 365000,
+    'IncludeDevelopMenu' => true,
+    'NewWindowBehavior' => 4,
+    'SendDoNotTrackHTTPHeader' => true,
+    'ShowFavoritesBar' => false,
+    'ShowFullURLInSmartSearchField' => true,
+    'TabCreationPolicy' => 2
+  }
 }
