df1611695d3b9bf07ef79f633796a880dc084ddc
Jeroen Jacobs <git@jeroenj.be>
2018-12-31 15:27:28 +0100

    [safari] Updates preferences

--- site-cookbooks/base/attributes/safari.rb
+++ site-cookbooks/base/attributes/safari.rb
@@ -1,13 +1,17 @@
 default[:base][:safari][:plist_settings] = {
   container: 'com.apple.Safari',
   settings: {
+    'AutoFillCreditCardData' => false,
+    'AutoFillFromAddressBook' => false,
+    'AutoFillMiscellaneousForms' => false,
     'AutoFillPasswords' => false,
     'AutoOpenSafeDownloads' => false,
     'DownloadsClearingPolicy' => 1,
     'FindOnPageMatchesWordStartsOnly' => false,
-    'HistoryAgeInDaysLimit' => 365000,
+    'HistoryAgeInDaysLimit' => 365,
     'IncludeDevelopMenu' => true,
     'NewWindowBehavior' => 4,
+    'PreloadTopHit' => false,
     'SendDoNotTrackHTTPHeader' => true,
     'ShowFavoritesBar' => false,
     'ShowFullURLInSmartSearchField' => true,
