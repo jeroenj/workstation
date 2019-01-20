d4b03e8431d782ea929ddde816728ff241caff32
Jeroen Jacobs <git@jeroenj.be>
2019-01-20 20:38:46 +0100

    [mail] Uses base_plist_settings resource instead of plist_settings recipe for settings

--- site-cookbooks/base/attributes/mail.rb
+++ site-cookbooks/base/attributes/mail.rb
@@ -1,13 +1,15 @@
-default[:base][:settings][:mail] = {
-  'domain' => "#{ENV['HOME']}/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail.plist",
-  'AutoReplyFormat' => true,
-  'EnableContactPhotos' => true,
-  'EnableToCCInMessageList' => true,
-  'IndexJunk' => true,
-  'JunkMailBehavior' => 2,
-  'NumberOfSnippetLines' => 0,
-  'PollTime' => 1,
-  'ShouldShowUnreadMessagesInBold' => true,
-  'SuppressDeliveryFailure' => 1,
-  'ThreadingDefault' => false
+default[:base][:mail][:plist_settings] = {
+  container: 'com.apple.mail',
+  settings: {
+    'AutoReplyFormat' => true,
+    'EnableContactPhotos' => true,
+    'EnableToCCInMessageList' => true,
+    'IndexJunk' => true,
+    'JunkMailBehavior' => 2,
+    'NumberOfSnippetLines' => 0,
+    'PollTime' => 1,
+    'ShouldShowUnreadMessagesInBold' => true,
+    'SuppressDeliveryFailure' => 1,
+    'ThreadingDefault' => false
+  }
 }
