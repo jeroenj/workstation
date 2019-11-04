46a8ca54cf7ed1e9bb2d5ac96d91ef7631732116
Jeroen Jacobs <git@jeroenj.be>
2019-11-04 21:05:14 +0100

    [zsh] Stops setting the default shell to zsh as that's the default in macOS now

--- site-cookbooks/base/recipes/zsh.rb
+++ site-cookbooks/base/recipes/zsh.rb
@@ -1,10 +1,5 @@
 package 'zsh'
 
-execute 'Set default shell to zsh' do
-  command "chsh -s /bin/zsh #{node[:base][:username]}"
-  not_if { `printf $SHELL` == '/bin/zsh' }
-end
-
 template "#{ENV['HOME']}/.profile" do
   source 'zsh/profile.erb'
   variables chef_env: node[:base][:chef][:client][:default_environment]
