ade0a30e24cb66b42bd06025f43fa8d58537292d
Jeroen Jacobs <git@jeroenj.be>
2020-01-26 12:26:01 +0100

    [ruby] Stops installing various default gems

--- site-cookbooks/base/attributes/ruby.rb
+++ site-cookbooks/base/attributes/ruby.rb
@@ -7,11 +7,7 @@ default[:base][:ruby] = {
   ),
   default: '2.6.5',
   gems: [
-    { name: 'bundler', version: '~> 1.17.3' },
-    { name: 'bundler-audit', version: '~> 0.6.1' },
-    { name: 'explain_shell' },
     { name: 'foreman' },
     { name: 'pry', version: '~> 0.12.2' },
-    { name: 'rubocop', version: '~> 0.48.1' }
   ]
 }
