45388d12414e027b93417369840508fc2f1d3808
Jeroen Jacobs <git@jeroenj.be>
2020-05-02 22:29:00 +0200

    [node] Removes recipe

--- site-cookbooks/base/attributes/node.rb
+++
@@ -1,1 +0,0 @@
-default[:base][:node][:packages] = %w(bower coffee-script js2coffee)
--- site-cookbooks/base/recipes/node.rb
+++
@@ -1,9 +0,0 @@
-package 'node'
-
-node[:base][:node][:packages].each do |package|
-  execute "npm install -g #{package}" do
-    user node[:base][:username]
-    group node[:base][:group]
-    not_if "npm list #{package} version -g --silent > /dev/null"
-  end
-end
