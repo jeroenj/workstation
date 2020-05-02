18d87611cf29c4e7d8a908e6ef7db10cf1ed0d4e
Jeroen Jacobs <git@jeroenj.be>
2020-05-02 16:59:59 +0200

    [tmux] Stops installing reattach-to-user-namespace as it's no longer needed with tmux 2.6+
    https://github.com/tmux/tmux/commit/78352fdd328b611d935ba051288191b33278cc4a

--- site-cookbooks/base/recipes/tmux.rb
+++ site-cookbooks/base/recipes/tmux.rb
@@ -1,8 +1,5 @@
 package 'tmux'
 
-# Fix copy-paste on macOS Sierra
-package 'reattach-to-user-namespace'
-
 cookbook_file "#{ENV['HOME']}/.tmux.conf" do
   source 'tmux/tmux.conf'
   owner node[:base][:username]
