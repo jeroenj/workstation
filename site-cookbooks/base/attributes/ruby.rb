c423a60b690ec3d12958744a4246c92addefd510
Jeroen Jacobs <git@jeroenj.be>
2020-10-20 22:02:05 +0200

    [ruby] Installs ruby 2.6.6 and 2.7.2 and marks 2.6.6 as default

--- site-cookbooks/base/attributes/ruby.rb
+++ site-cookbooks/base/attributes/ruby.rb
@@ -1,11 +1,10 @@
 default[:base][:ruby] = {
   path: '/opt/rubies',
   rubies: %w(
-    2.7.0
-    2.6.5
-    2.4.9
+    2.7.1
+    2.6.6
   ),
-  default: '2.6.5',
+  default: '2.6.6',
   gems: [
     { name: 'foreman' },
     { name: 'pry', version: '~> 0.13.1' },
