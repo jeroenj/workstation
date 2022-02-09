796094d4c46293287dc9af532aeeb5be5ddedebc
Jeroen Jacobs <git@jeroenj.be>
2022-02-09 23:03:02 +0100

    [dodyndns] Updates to 1.0.0

--- site-cookbooks/base/attributes/dodyndns.rb
+++ site-cookbooks/base/attributes/dodyndns.rb
@@ -1,6 +1,6 @@
-default[:base][:dodyndns][:package][:version] = '0.1.1'
+default[:base][:dodyndns][:package][:version] = '1.0.0'
 default[:base][:dodyndns][:package][:url] = "https://github.com/jeroenj/dodyndns/releases/download/#{node[:base][:dodyndns][:package][:version]}/dodyndns-#{node[:base][:dodyndns][:package][:version]}.darwin-amd64"
-default[:base][:dodyndns][:package][:checksum] = '06fe637ce6a177178143828ceee9ee4caa14992036f8e43781552b7ca564320f'
+default[:base][:dodyndns][:package][:checksum] = '320969ee4ae241f35dc1befefd9910bc418cf5013840fad39ff9999dcc0887bb'
 
 default[:base][:dodyndns][:domain] = nil
 default[:base][:dodyndns][:name] = node[:base][:hostname]
